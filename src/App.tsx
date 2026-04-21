a4b23a5f2cdf75171c30bb65868ad9c17ea66a01
idgmedia2025-app <idgmedia2025@gmail.com>
2026-04-22 02:15:03 +0300

    Implement ScrollToTop component for navigation
    Added ScrollToTop component to reset scroll position on route change.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
@@ -6,9 +7,23 @@ import SolutionsPage from './pages/SolutionsPage'
 import FeaturesPage from './pages/FeaturesPage'
 import PricingPage from './pages/PricingPage'
 
+// Component yar oo Scroll-ka kor u celinaya
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 export default function App() {
   return (
     <div className="min-h-screen bg-dark-900 flex flex-col">
+      {/* Tan ayaa maamulaysa in kor lagugu celiyo */}
+      <ScrollToTop />
+      
       <Navbar />
       <main className="flex-1">
         <Routes>
